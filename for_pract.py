678271ab23b4a636572d024713820507de178edc
baba shankar <babashankar123@gmail.com>
2024-01-02 08:50:23 +0530

    loop practice2

--- for_pract.py
+++ for_pract.py
@@ -1,34 +1,34 @@
-# inp = ['india','uk','austria','germany','usa']
-#
-# l=int(input('enter length'))
-# for i in inp:
-#     if len(i)==l:
-#         print(i)
+inp = ['india','uk','austria','germany','usa']
 
-#
-# inp='a6g4a3j3'
-# for i in inp:
-#     if (not i.isdigit()):
-#         k=i
-#     else:
-#         for c in range(int(i)):
-#             print(k,end="")
+l=int(input('enter length'))
+for i in inp:
+    if len(i)==l:
+        print(i)
 
-# inp='a6g4a3j3'
-# for i in range(0,len(inp),2):
-# #     print(inp[i] * int(inp[i+1]),end='')
-# #
-#
-#
-# inp = 6
-# for i in range(inp):
-#     print(' '* int((inp-i)/2),end='')
-#     print('*' * i)
-#     # print('*'* (i-1))
-# for i in range(int,0,-1):
-#     print(' '* int((inp-i)/2),end='')
-#     print('*' * i)
-#     # print('*'* (i-1))
+
+inp='a6g4a3j3'
+for i in inp:
+    if (not i.isdigit()):
+        k=i
+    else:
+        for c in range(int(i)):
+            print(k,end="")
+
+inp='a6g4a3j3'
+for i in range(0,len(inp),2):
+    print(inp[i] * int(inp[i+1]),end='')
+
+
+
+inp = 6
+for i in range(inp):
+    print(' '* int((inp-i)/2),end='')
+    print('*' * i)
+    # print('*'* (i-1))
+for i in range(int,0,-1):
+    print(' '* int((inp-i)/2),end='')
+    print('*' * i)
+    # print('*'* (i-1))
 
 
 n=7
